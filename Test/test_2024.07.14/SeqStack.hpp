311b886aa2a759109efb4edca360934c01fe01d1
Seraphimu <serahuimu@outlook.com>
2024-07-17 08:51:28 +0800

    Clean up

--- Test/test_2024.07.14/SeqStack.hpp
+++ Test/test_2024.07.14/SeqStack.hpp
@@ -7,15 +7,23 @@ typedef int DataType;
 
 class SeqStack {
 public:
+    //数组
     DataType * A;
+    //栈顶
     int Top;
 
+    //建立一个空栈
     void Init();
+    //把X入栈
     void Push(DataType X);
+    //出栈
     void Pop();
+    //获取栈顶元素
     DataType GetTop();
+    //摧毁栈
     void MakeEmpty();
 
+    //判空，判满
     bool IsEmpty();
     bool IsFull();
 };
