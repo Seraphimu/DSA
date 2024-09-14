8e9d737b620959f2980cc0e146096667082688c3
Seraphimu <Seraphimu@outlook.com>
2024-09-14 03:00:16 +0000

    PushFront

--- CLinerList/SingleLinkedList/SL.c
+++ CLinerList/SingleLinkedList/SL.c
@@ -1,5 +1,31 @@
 #include "SL.h"
 
-void SLInit(Node ** pphead) {
-    *pphead = NULL;
+void SLInit(Node ** PPHead) {
+    *PPHead = NULL;
+}
+
+//分配节点函数
+Node * SLBuyNode(DataType x) {
+	Node * newNode = (Node *)malloc(sizeof(Node));
+	//分配失败
+	if (newNode == NULL) {
+		printf("Malloc failed!\n");
+		exit(-1);
+	}
+
+	//能走到这里，程序一切正常，分配成功
+	newNode->data = x;
+	newNode->next = NULL;
+
+	return newNode;
+}
+
+//头插法
+void SLPushBack(Node ** PPHead, DataType x) {
+	//分配节点
+	Node * newNode = SLBuyNode(x);
+	//新节点指针域指向头指针
+	newNode->next = *PPHead;
+	//重置头指针为newNode
+	*PPHead = newNode;	
 }
--- CLinerList/SingleLinkedList/Test.c
+++ CLinerList/SingleLinkedList/Test.c
@@ -1,8 +1,13 @@
 #include "SL.h"
 
 void test() {
-    Node * plist;       //此时plist应该存放了一个随机值
-    SLInit(&plist);     //在调用SLInit之后plist会变成空
+    Node * PList;       //此时plist应该存放了一个随机值
+    SLInit(&PList);     //在调用SLInit之后plist会变成空
+	SLPushBack(&PList, 1);
+	SLPushBack(&PList, 2);
+	SLPushBack(&PList, 3);
+	SLPushBack(&PList, 4);
+	SLPushBack(&PList, 5);
 }
 
 int main(void) {
